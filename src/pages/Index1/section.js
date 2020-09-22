6b0d531068856173387928c022439d1b3c374e50
PedroManuelJM <mpedrojurado@gmail.com>
2020-09-22 16:49:11 -0500

    agregando link de github

--- src/pages/Index1/section.js
+++ src/pages/Index1/section.js
@@ -20,11 +20,10 @@ class Section extends Component {
             <React.Fragment>
                 <section className="hero-section" id="home">     
                                 <section className="imagen-section" id="imagen">
-                                <Container>
-                                    <h2>bienvenidaa</h2>
+                                <Container>                             
                                     <Row className="justify-content-center">     
                             
-                                    <Button href="https://www.online-convert.com/es/resultado/ed7c9b67-ef66-4408-9d77-6297fc8e3ca0" className="btn btn-primary"  target="_blank" >Mi Github</Button>
+                                    <Button href="https://github.com/PedroManuelJM?tab=repositories" className="btn btn-primary"  target="_blank" >Mi Github</Button>
                                     </Row>    
                                 </Container>
                                 </section>     
