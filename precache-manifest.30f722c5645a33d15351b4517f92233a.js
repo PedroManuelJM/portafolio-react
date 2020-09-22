61a58d204ebbafd7c88b4b64d870fa11a5efab1c
PedroManuelJM <mpedrojurado@gmail.com>
2020-09-22 15:11:01 -0500

    Updates

---
+++ precache-manifest.30f722c5645a33d15351b4517f92233a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3deb95544882e0bd6341f5af87d33255",
+    "url": "/portafolio-react/index.html"
+  },
+  {
+    "revision": "5c84d13c857a0bbbff38",
+    "url": "/portafolio-react/static/css/2.828be9a0.chunk.css"
+  },
+  {
+    "revision": "85ffbe2c7285eb3eaa5e",
+    "url": "/portafolio-react/static/css/main.e05f777e.chunk.css"
+  },
+  {
+    "revision": "5c84d13c857a0bbbff38",
+    "url": "/portafolio-react/static/js/2.285bcdfd.chunk.js"
+  },
+  {
+    "revision": "413b595604c5d3d1d7baf18f18130db2",
+    "url": "/portafolio-react/static/js/2.285bcdfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ffbe2c7285eb3eaa5e",
+    "url": "/portafolio-react/static/js/main.e1dac057.chunk.js"
+  },
+  {
+    "revision": "d2cf96da7a7c80889b4c",
+    "url": "/portafolio-react/static/js/runtime-main.3d839029.js"
+  },
+  {
+    "revision": "4570c307e0a3d29c7b18854540b2a4cf",
+    "url": "/portafolio-react/static/media/img-2.4570c307.png"
+  },
+  {
+    "revision": "3ac50b5b36eb2f11b000dce1792d0bb0",
+    "url": "/portafolio-react/static/media/materialdesignicons-webfont.3ac50b5b.ttf"
+  },
+  {
+    "revision": "7ec5dab7e7ff250971d2ff50379778dc",
+    "url": "/portafolio-react/static/media/materialdesignicons-webfont.7ec5dab7.woff2"
+  },
+  {
+    "revision": "a0d13d16cc2f3647680d9f1ff003f58b",
+    "url": "/portafolio-react/static/media/materialdesignicons-webfont.a0d13d16.woff"
+  },
+  {
+    "revision": "a32fa1f27abbfa96ff2f79e1ade723d5",
+    "url": "/portafolio-react/static/media/materialdesignicons-webfont.a32fa1f2.eot"
+  },
+  {
+    "revision": "91b1330a728bad4d705bf8016b36d1a7",
+    "url": "/portafolio-react/static/media/portafolio2.91b1330a.png"
+  }
+]);
